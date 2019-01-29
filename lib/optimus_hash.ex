3fbe129a07fe6c52509e7795c8b73e6bc51c1526
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2019-01-29 14:45:16 +0100

    feat: Add better validation logic to `new/1`

--- lib/optimus_hash.ex
+++ lib/optimus_hash.ex
@@ -36,9 +36,21 @@ defmodule OptimusHash do
     random = Keyword.get(opts, :random)
     max_int = Keyword.get(opts, :max_int, 2_147_483_647)
 
+    if prime >= max_int do
+      raise ArgumentError, "Argument :prime is larger or equal to :max_int"
+    end
+
+    if random >= max_int do
+      raise ArgumentError, "Argument :random is larger or equal to :max_int"
+    end
+
     if Keyword.get(opts, :validate, true) do
       if !is_integer(prime) || !Helpers.is_prime?(prime) do
         raise ArgumentError, "Argument :prime is not a prime number"
+      end
+
+      if (prime * mod_inverse &&& max_int) !== 1 do
+        raise ArgumentError, "Argument :mod_inverse is invalid"
       end
     end
 
@@ -46,18 +58,28 @@ defmodule OptimusHash do
   end
 
   @doc """
-  Encodes the given number.
+  Encodes the given number and returns the result.
+
+      iex> OptimusHash.encode(o, 1)
+      458_047_115
+
   """
-  def encode(o, number) do
+  @spec encode(OptimusHash.t(), non_neg_integer) :: non_neg_integer
+  def encode(o, number) when is_integer(number) do
     (number * o.prime &&& o.max_int) ^^^ o.random
   end
 
   def encode(_, _), do: nil
 
   @doc """
-  Encodes the given number.
+  Decodes the given number and returns the result.
+
+      iex> OptimusHash.decode(o, 458_047_115)
+      1
+
   """
-  def decode(o, number) do
+  @spec decode(OptimusHash.t(), non_neg_integer) :: non_neg_integer
+  def decode(o, number) when is_integer(number) do
     (number ^^^ o.random) * o.mod_inverse &&& o.max_int
   end
 
--- test/optimus_hash_test.exs
+++ test/optimus_hash_test.exs
@@ -1,6 +1,4 @@
 defmodule OptimusHashTest do
-  alias OptimusHash
-
   use ExUnit.Case
 
   describe "new()" do
@@ -13,22 +11,53 @@ defmodule OptimusHashTest do
 
       o =
         OptimusHash.new(
-          prime: 435_168_289,
-          mod_inverse: 1_166_565_345,
-          random: 1_831_981_360,
-          max_int: 9_223_372_036_854_775_807
+          prime: 4_125_641_476_418_359_867,
+          mod_inverse: 3_805_797_621_438_101_235,
+          random: 3_645_042_782,
+          max_int: 4_611_686_018_427_387_903
         )
 
-      assert o.max_int == 9_223_372_036_854_775_807
+      assert o.max_int == 4_611_686_018_427_387_903
     end
 
     test "raises when provided with an non-prime number" do
-      assert_raise ArgumentError, fn ->
+      assert_raise ArgumentError, "Argument :prime is not a prime number", fn ->
         OptimusHash.new(prime: 1, mod_inverse: 59_260_789, random: 1_163_945_558)
       end
 
       # This should not raise
-      OptimusHash.new(prime: 1, mod_inverse: 59_260_789, random: 1_163_945_558, validate: false)
+      OptimusHash.new(
+        prime: 1,
+        mod_inverse: 2,
+        random: 1_163_945_558,
+        validate: false
+      )
+    end
+
+    test "raises when provided an invalid mod_inverse" do
+      assert_raise ArgumentError, "Argument :mod_inverse is invalid", fn ->
+        OptimusHash.new(prime: 1_580_030_173, mod_inverse: 1, random: 1_163_945_558)
+      end
+    end
+
+    test "raises when provided a prime number larger than :max_int" do
+      assert_raise ArgumentError, "Argument :prime is larger or equal to :max_int", fn ->
+        OptimusHash.new(
+          prime: 67_280_421_310_721,
+          mod_inverse: 1_718_041_753,
+          random: 1_163_945_558
+        )
+      end
+    end
+
+    test "raises when provided a random number larger than :max_int" do
+      assert_raise ArgumentError, "Argument :random is larger or equal to :max_int", fn ->
+        OptimusHash.new(
+          prime: 1_580_030_173,
+          mod_inverse: 59_260_789,
+          random: 9_223_372_036_854_775_807
+        )
+      end
     end
   end
 
